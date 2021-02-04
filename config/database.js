74a839be84a800a3196e7f2c133dc8ad70e5af6d
Resilient923 <64014651+Resilient923@users.noreply.github.com>
2021-02-05 02:07:01 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'rp.c4wybh857q9a.ap-northeast-2.rds.amazonaws.com',
     user: 'steady',
     port: 3306,
-    password: 'Dlstkgodydkssud0',
+    password: '',
     database: 'RP_aws'
 });
 
